f67f2c388960e6404d64e3a7f339bf566ffb5cd3
Huy Nguyen <huynt57@gmail.com>
2016-07-04 15:34:05 +0700

    moss

--- controllers/MossController.php
+++ controllers/MossController.php
@@ -2,25 +2,11 @@
 
 namespace app\controllers;
 
-use app\components\MOSS;
-
-class MossController extends \yii\web\Controller {
-
-    public function actionIndex() {
+class MossController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
         return $this->render('index');
-    }
-
-    public function actionUpload() {
-        
-    }
-
-    public function actionCheck() {
-        $moss = new MOSS(Yii::$app->params['moss_user_id']);
-        $moss->setLanguage($lang);
-        $moss->addByWildcard('test/*');
-        $moss->addBaseFile('Example.java');
-        $moss->setCommentString("This is a test");
-        $moss->send();
     }
 
 }
