2de606cd907bbccb9f2781055f9c4e744244dffb
Huy Nguyen <huynt57@gmail.com>
2016-07-20 11:18:08 +0700

    set default controller

--- config/web.php
+++ config/web.php
@@ -69,7 +69,8 @@ $config = [
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
             ],
-        ]
+        ],
+        'defaultRoute' => 'document/get-latest-documents',
     ],
     'params' => $params,
 ];
